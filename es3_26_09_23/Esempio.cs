3e6b4d11a3a83556640607672f0a97b5b1c80deb
lorenzodamora <damora.lorenzo.studente@itispaleocapa.it>
2023-10-16 17:02:31 +0200

    Aggiungere i file di progetto.

---
+++ es3_26_09_23/Esempio.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace es3_26_09_23
+{
+	internal class Esempio : IDisposable
+	{
+
+		private string _nome;
+		private bool _disposed;
+
+		public string Nome
+		{
+			get { return _nome; }
+			set { _nome = value; }
+		}
+
+		public Esempio(string n = "nome")
+		{
+			Nome = n;
+		}
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if(!_disposed)
+			{
+				if(disposing)
+				{
+					Console.WriteLine($"Oggetto {Nome} Eliminato");
+					// rilascia le risorse gestite qui (qual'ora ci fossero)
+				}
+				// rilascia le risorse non gestite qui (qual'ora ci fossero)
+
+				_disposed = true;
+			}
+		}
+
+		public void Visualizza()
+		{
+			Console.WriteLine("Nome: " + _nome);
+		}
+
+		//auto run se passa per il garbage collector GC, il GC decide se e quando c'è bisogno di distruggere l'oggetto
+		// forzabile con il metodo Collect (dopo aver impostasto l'oggetto a null)
+		// se passa prima per Dispose che ha 'GC.SuppressFinalize(this);' allora non passa per il finalizer
+		~Esempio() // finalizer (vecchio nome: distruttore)
+		{
+			Dispose(false);
+			Console.WriteLine("Distruttore di " + Nome);
+		}
+
+	}
+}
---
+++ es3_26_09_23/Starter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data.Common;
+
+namespace es3_26_09_23
+{
+	internal class Starter
+	{
+		static void Main()
+		{
+			using(Esempio e0 = new Esempio("num 0")) { }
+			Console.WriteLine("0: Con il blocco using passa dal metodo Dispose.\n");
+
+			Esempio e1 = new Esempio("num 1");
+			e1.Dispose();
+			Console.WriteLine("1: Con il metodo Dispose() passa dal metodo virtual Dispose(bool disposing) con parametro true.\n");
+
+			Esempio e2 = new Esempio("num 2");
+			e2 = null;
+			Console.WriteLine("2: Impostando l'oggetto a 'null' e aspettando un tempo indefinito,\n Garbage Collector userà il metodo finalizer(prima conosciuto come distruttore) ~Class \n");
+
+			Esempio e3 = new Esempio("num 3");
+			e1= e3 = null;
+			GC.Collect();
+			Console.WriteLine("3: Impostando l'oggetto a 'null' e usando il metodo GC.Collect(),\n Garbage Collector si avvierà una volta senza aspettare\n");
+		}
+	}
+}
